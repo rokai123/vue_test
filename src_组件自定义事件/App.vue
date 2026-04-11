7424e06c5af10001579a4494be9ea9c9fa5abc54
lukai <lu8508598@gmail.com>
2026-04-11 14:57:56 +0900

    自定义组件事件

--- src_组件自定义事件/App.vue
+++ src_组件自定义事件/App.vue
@@ -1,9 +1,9 @@
 <template>
 	<div>
-		<h1 class="title">你好啊</h1>
+		<h1 class="title">你好啊{{stuName}}</h1>
 		<MySchool :getSchoolName="getSchoolName"/>
 		<!-- <MyStudent @atguigu="getStudentName"/> -->
-		<MyStudent :ref="student"/>
+		<MyStudent ref="student" @click.native="show"/><!--@click.native="show"绑定原生事件-->
 	</div>
 </template>
 
@@ -14,21 +14,35 @@
 	export default {
 		name:'App',
 		components:{MySchool,MyStudent},
+		data(){
+			return{
+				stuName:''
+			}
+		},
 		methods:{
-			getStudentName(stuName){
-				alert('父组件接收到的学生姓名是：'+stuName)
+			getStudentName(stuName,...params){
+				this.stuName = stuName
+				console.log('额外参数：',params)
+				this.stuName = stuName
 			},
 			getSchoolName(schoolName){
 				alert('父组件接收到的学校名称是：'+schoolName)
+			},
+			show(){
+				alert('点击了学生组件')
 			}
 		},
 		mounted(){
-			setTimeout(()=>{
+			/* setTimeout(()=>{
 				//写法更灵活
 				// this.$refs.student.$on('atguigu',this.getStudentName)
 				this.$refs.student.$once('atguigu',this.getStudentName)
-			},3000)
-			
+			},3000) */
+			this.$refs.student.$on('atguigu',this.getStudentName)
+			/* this.$refs.student.$on('atguigu',(name,...params)=>{
+				console.log('额外参数：',params)
+				this.stuName = name
+			}) */
 		}
 	}
 </script>
--- src_组件自定义事件/components/Student.vue
+++ src_组件自定义事件/components/Student.vue
@@ -3,6 +3,7 @@
 		<h2 class="title">学生姓名：{{name}}</h2>
 		<h2 class="atguigu">学生性别：{{sex}}</h2>
 		<button @click="sendStuNameToApp">点击我把学生姓名传给父组件</button>
+		<button @click="unBind">点我解绑自定义事件</button>
 	</div>
 </template>
 
@@ -18,6 +19,12 @@
 		methods:{
 			sendStuNameToApp(){
 				this.$emit('atguigu',this.name,'123','456')
+			},
+			unBind(){
+				this.$off('atguigu')
+				// this.$off() //解绑所有事件
+				// this.$off('atguigu',['atguigu', 'llll']) //解绑指定事件
+				console.log('解绑成功')
 			}
 		}
 	}
